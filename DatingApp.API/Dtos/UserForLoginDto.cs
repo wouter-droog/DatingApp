b839e9e6e01d3960d66f66d08c02bf550adfdaaf
Wouter Droog <wouterd@beround.nl>
2018-10-26 10:48:13 +0200

    End section 3

---
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,12 @@
+
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+
+
+        public string Password { get; set; }
+    }
+}
